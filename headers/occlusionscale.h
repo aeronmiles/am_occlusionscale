64aa6e55c4205c5f2376b7a2a022aeb902242237
aeronmiles <aeronmiles@yahoo.co.uk>
2016-04-14 13:34:53 +0100

    master

--- headers/occlusionscale.h
+++ headers/occlusionscale.h
@@ -140,17 +140,3 @@ public:
 		return LXe_OK;
 	}
 };
-
-//Test code
-//bb[0][0] = bb_min_a[0];
-//bb[0][1] = bb_min_a[1];
-//bb[0][2] = bb_min_a[2];
-//bb[1][0] = bb.min[0];
-//bb[1][1] = bb.min[1];
-//bb[1][2] = bb.min[2];
-//bb[2][0] = bb_max_a[0];
-//bb[2][1] = bb_max_a[1];
-//bb[2][2] = bb_max_a[2];
-//bb[3][0] = bb_max_b[0];
-//bb[3][1] = bb_max_b[1];
-//bb[3][2] = bb_max_b[2];
